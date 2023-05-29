ecc3992016d9b6651024817b00026328cb457cd5
ValentinoFeru <valmaster16@gmail.com>
2023-05-29 00:15:16 -0300

    correcciones

--- Desafio10/routers/chatRouter.js
+++
@@ -1,9 +0,0 @@
-const server = require("express").Router();
-
-server.get("/", (req, res) => {
-  if (req.session.user) {
-    return res.render("chat.hbs");
-  } else return res.redirect("login");
-});
-
-module.exports = server;
